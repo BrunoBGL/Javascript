70a9bb31f7f77ca98dd9c7cbaedf2e22436c58ad
Bruno Gustavo <bruno2011gustavo@hotmail.com>
2017-11-07 09:36:23 -0200

    Aula 2 - Variaveis e Operadores

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,31 @@
 
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+
+var altura = paciente.querySelector(".info-altura").textContent;
+
+var pesoValido = true;
+var alturaValido = true;
+var tdImc =  paciente.querySelector(".info-imc");
+
+if(peso <=0 || peso>=1000){
+  pesoValido = false;
+  tdImc.textContent = "Peso Inválido";
+}
+
+if (altura <=0 || altura >=3){
+  alturaValido = false;
+  tdImc.textContent = "Altura Inválida";
+}
+
+if(alturaValido && pesoValido){
+  var imc = peso / (altura * altura);
+  tdImc.textContent = imc;
+}
+
+
+console.log(imc);
