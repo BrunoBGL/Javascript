042245abc4c9e0a0b51cf4ab449ed2b77db16f0a
Bruno Gustavo <bruno2011gustavo@hotmail.com>
2017-11-14 16:05:13 -0200

    Aula 4 -  Eventos, Formulários e Criando Elementos

--- js/principal.js
+++ js/principal.js
@@ -1,31 +1,35 @@
 
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+  var titulo = document.querySelector(".titulo");
+  titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+  var pacientes = document.querySelectorAll(".paciente");
 
-var peso = paciente.querySelector(".info-peso").textContent;
+for(var i=0; i<=pacientes.length  ; i++){
 
-var altura = paciente.querySelector(".info-altura").textContent;
+  var paciente = pacientes[i]
+  var peso = paciente.querySelector(".info-peso").textContent;
+  var altura = paciente.querySelector(".info-altura").textContent;
+  var pesoValido = true;
+  var alturaValido = true;
+  var tdImc =  paciente.querySelector(".info-imc");
 
-var pesoValido = true;
-var alturaValido = true;
-var tdImc =  paciente.querySelector(".info-imc");
 
-if(peso <=0 || peso>=1000){
-  pesoValido = false;
-  tdImc.textContent = "Peso Inválido";
+  if(peso <=0 || peso>=1000){
+    pesoValido = false;
+    tdImc.textContent = "Peso Inválido";
+    paciente.classList.add("paciente-invalido");
+    }
+
+  if (altura <=0 || altura >=3){
+    alturaValido = false;
+    tdImc.textContent = "Altura Inválida";
+    paciente.classList.add("paciente-invalido");
+
+  }
+
+  if(alturaValido && pesoValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
-
-if (altura <=0 || altura >=3){
-  alturaValido = false;
-  tdImc.textContent = "Altura Inválida";
-}
-
-if(alturaValido && pesoValido){
-  var imc = peso / (altura * altura);
-  tdImc.textContent = imc;
-}
-
-
-console.log(imc);
+  console.log(imc);
